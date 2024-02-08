ef4738844e8f43b15101ed65c5db9f10975a3117
Mikhail <mgubankov@yandex.ru>
2024-02-08 12:52:01 +0300

    timer update

--- js/script.js
+++ js/script.js
@@ -44,11 +44,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const deadline = '2024-03-19';
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 *60)) % 24),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor ((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 *60)) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor ((t / 1000) % 60);
+        }
 
         return {
             total: t,
@@ -71,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         function getZero(num) {
             if (num >= 0 && num < 10) {
-                return `0${mun}`
+                return `0${num}`
             } else {
                 return num;
             }
